15c88553b339ca8f4a8e985668e9b1d8387ade56
Mahmoud Bander <bandermahmoud3@gmail.com>
2024-08-25 03:47:59 +0200

    Date Time App Using Intl

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, override_on_non_overriding_member, unused_import
 
-
-
 import 'package:flutter/material.dart';
 import "dart:async";
+import 'package:intl/intl.dart'; 
 
 void main() {
   runApp(const MyApp());
@@ -30,95 +29,16 @@ class DateApp extends StatefulWidget {
 }
 
 class _DateAppState extends State<DateApp> {
-  String year = "";
-  String month = "";
-  String dayNumber = "";
-  String dayweek = "";
-  // Time
-  int hour = 0;
-  String min = "";
-  String sce = "";
-  // Date
-  String pmORam = "AM";
+  String weekday = "";
+  String date = "";
+  String timeNow = "";
+
   changeEverySec() {
     Timer.periodic(Duration(seconds: 1), (Timer) {
       setState(() {
-        // Time
-        hour = DateTime.now().hour;
-        if (hour > 12) {
-          hour = hour - 12;
-          pmORam = "PM";
-        }
-        min = DateTime.now().minute.toString();
-        sce = DateTime.now().second.toString();
-        // Date
-        year = DateTime.now().year.toString();
-        month = DateTime.now().month.toString();
-        switch (month) {
-          case "1":
-            month = "January";
-            break;
-          case "2":
-            month = "February";
-            break;
-          case "3":
-            month = "March";
-            break;
-          case "4":
-            month = "April";
-            break;
-          case "5":
-            month = "May";
-            break;
-          case "6":
-            month = "June";
-            break;
-          case "7":
-            month = "July";
-            break;
-          case "8":
-            month = "August";
-            break;
-          case "9":
-            month = "September";
-            break;
-          case "10":
-            month = "October";
-            break;
-          case "11":
-            month = "November";
-            break;
-          case "12":
-            month = "December";
-            break;
-        }
-
-        dayNumber = DateTime.now().day.toString();
-        dayweek = DateTime.now().weekday.toString();
-
-        switch (dayweek) {
-          case "1":
-            dayweek = "Monday";
-            break;
-          case "2":
-            dayweek = "Tuesday";
-            break;
-          case "3":
-            dayweek = "Wednesday";
-            break;
-          case "4":
-            dayweek = "Thursday";
-            break;
-          case "5":
-            dayweek = "Friday";
-            break;
-          case "6":
-            dayweek = "Saturday";
-            break;
-          case "7":
-            dayweek = "Sunday";
-            break;
-        }
+        weekday =(DateFormat("EEEE").format(DateTime.now()));
+        date = (DateFormat("MMMM d, y").format(DateTime.now()));
+        timeNow = (DateFormat("hh : mm : ss a").format(DateTime.now()));
       });
     });
   }
@@ -149,21 +69,21 @@ class _DateAppState extends State<DateApp> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              "Todays Date is $dayweek",
+              "Todays Date is $weekday",
               style: TextStyle(fontSize: 30, color: Colors.white),
             ),
             SizedBox(
               height: 20,
             ),
             Text(
-              "$month $dayNumber, $year",
+              date,
               style: TextStyle(fontSize: 30, color: Colors.white),
             ),
             SizedBox(
               height: 20,
             ),
             Text(
-              "${hour.toString().padLeft(2, "0")}:${min.padLeft(2, "0")}:${sce.padLeft(2, "0")}    $pmORam",
+              timeNow,
               style: TextStyle(fontSize: 30, color: Colors.white),
             ),
             SizedBox(
